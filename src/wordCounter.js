03045c392620b81dface6ebd30af5828c329f170
Skander Mejdi <mejdi.skander@gmail.com>
2017-12-01 21:41:32 +0100

    punctuation / lower case

--- src/wordCounter.js
+++ src/wordCounter.js
@@ -1,9 +1,11 @@
 function wordCounter(text, minLength) {
 
-  var wordTab = text.split(" ");
+  var punctuationless = text.replace(/[.,\/#!?$%\^&\*;:{}=\-_`~()]/g,"");
+  var wordTab = punctuationless.split(" ");
   var countTab = {};
 
   for (var i = 0; i < wordTab.length; i++) {
+    wordTab[i] = wordTab[i].toLowerCase();
     if (wordTab[i].length < minLength) {
       wordTab.splice(i--, 1);
     } else {
