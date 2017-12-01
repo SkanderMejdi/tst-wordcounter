f6bb585d740dc9efcd79cb3ca4c9f09bd5feb229
Skander Mejdi <mejdi.skander@gmail.com>
2017-12-01 21:36:53 +0100

    premier jet

--- src/wordCounter.js
+++ src/wordCounter.js
@@ -1,8 +1,20 @@
 function wordCounter(text, minLength) {
 
-    //
+  var wordTab = text.split(" ");
+  var countTab = {};
 
-    return {};
+  for (var i = 0; i < wordTab.length; i++) {
+    if (wordTab[i].length < minLength) {
+      wordTab.splice(i--, 1);
+    } else {
+      if (countTab[wordTab[i]] == 1) {
+        countTab[wordTab[i]] += 1
+      } else {
+        countTab[wordTab[i]] = 1;
+      }
+    }
+  }
+  return countTab;
 }
 
 module.exports = wordCounter;
